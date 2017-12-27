5702020afd3cdc8911bfb6467d35c8bfcb5ff64e
wes <wes@home.local>
2017-12-26 20:15:49 -0600

    Tagging with version

--- amd/build.sh
+++ amd/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wesparish/claymore:amd . && \
-  docker push wesparish/claymore:amd 
+docker build -t wesparish/claymore:amd-v10.0 . && \
+  docker push wesparish/claymore:amd-v10.0
