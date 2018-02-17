013c370101750002039f3678259d6488bfdfabb7
wes <wes@home.local>
2018-02-17 07:09:55 -0600

    Bumping Claymore to v11 (non-nanopool release)

--- amd/build.sh
+++ amd/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t wesparish/claymore:amd-v10.0-1.2 . && \
-  docker push wesparish/claymore:amd-v10.0-1.2
+docker build -t wesparish/claymore:amd-v11.0-1 . && \
+  docker push wesparish/claymore:amd-v11.0-1
